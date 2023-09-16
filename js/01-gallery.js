d2fbbe634f8d489918761134107ebbff4b9c9916
Yakushyn Mark <yakushyn.mark@gmail.com>
2023-09-16 17:49:01 +0200

    created markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsMarkup = galleryItems
+  .map(el => {
+    const { description, original, preview } = el;
+
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li> `;
+  })
+  .join('');
+
+galleryEl.innerHTML = galleryItemsMarkup;

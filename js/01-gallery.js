988b7b5d2fba4887c5582a9763abacd44748dddd
Yakushyn Mark <yakushyn.mark@gmail.com>
2023-09-17 15:01:43 +0200

    finished the first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,63 @@
 import { galleryItems } from './gallery-items.js';
 
-// Change code below this line
 const galleryEl = document.querySelector('.gallery');
 
-const galleryItemsMarkup = galleryItems
-  .map(el => {
-    const { description, original, preview } = el;
+let instance = null;
 
-    return `<li class="gallery__item">
+const createMarkup = items => {
+  return items
+    .map(
+      ({ preview, description, original }) =>
+        `<li class="gallery__item">
   <a class="gallery__link" href="large-image.jpg">
     <img
       class="gallery__image"
       src="${preview}"
-      data-source="${original}"
+      data-source="${original}"git
       alt="${description}"
     />
   </a>
-</li> `;
-  })
-  .join('');
+</li> `
+    )
+    .join('');
+};
 
-galleryEl.innerHTML = galleryItemsMarkup;
+const addMarkup = items => (galleryEl.innerHTML = createMarkup(items));
+
+addMarkup(galleryItems);
+
+const escHandler = e => {
+  if (e.code !== 'Escape') {
+    return;
+  }
+
+  instance.close();
+};
+
+const createModal = e => {
+  instance = basicLightbox.create(
+    `
+ <img src="${e.target.dataset.source}" width="800" height="600">
+`,
+    {
+      onShow: instance => {
+        document.addEventListener('keydown', escHandler);
+      },
+      onClose: instance => {
+        document.removeEventListener('keydown', escHandler);
+      },
+    }
+  );
+
+  return instance;
+};
+
+const modalHandler = e => {
+  e.preventDefault();
+
+  const modal = createModal(e);
+
+  modal.show();
+};
+
+galleryEl.addEventListener('click', modalHandler);
